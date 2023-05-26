d764d78c6adf2a2bbd9764f4454ebece42439e32
student12 <133848904+Kripchek@users.noreply.github.com>
2023-05-26 14:16:37 +0300

    доделать

---
+++ ConsoleApp17/ConsoleApp17/Employees.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Employees
+    {
+        public string _fullName;
+        public int _department;
+        public double _salary;
+
+        public Employees(string fullname,int department,double salary) 
+        {
+            _fullName = fullname;
+            _department = department;
+            _salary = salary; 
+
+        }
+        public string FullName
+        {
+            get => _fullName;
+            set => _fullName = value;
+        }
+        public  int Department
+        {
+            get => _department;
+            set => _department = value;
+        }
+        public double Salary
+        { 
+            get => _salary;
+            set => _salary = value;
+        }
+        public void GetEmployeesInfo()
+        {
+            foreach (Employees emp in P)
+            {
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employees> employee = new List<Employees>()
+            {
+                new("блоб блоб",1,18800),
+                new("блаб блаб",1,14737),
+                new("блеб блеб",2,28800),
+                new("бляб бляб",2,24737),
+                new("бхоб бхоб",3,38800),
+                new("бхаб бхаб",3,34737),
+                new("бхеб бхеб",4,48800),
+                new("бхяб бхяб",4,44737),
+
+            };
+        }
+    }
+}
