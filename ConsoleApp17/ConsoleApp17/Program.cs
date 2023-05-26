a6c9191dc3f41cb2c8f7330640e8356692a80936
student12 <133848904+Kripchek@users.noreply.github.com>
2023-05-26 14:23:56 +0300

    доделать

--- ConsoleApp17/ConsoleApp17/Program.cs
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp17
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             List<Employees> employee = new List<Employees>()
             {
